4bcf9a6c8ec3d63583fd27b47a5d01f5678a6bd8
Quang <quangdilinh@gmail.com>
2020-02-08 01:37:46 +0700

    new file

---
+++ Assets/Scripts/player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    public float RunSpeed = 4;
+    Rigidbody2D myRigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Run();
+    }
+    void Run(){
+        float velocityX;
+        velocityX = Input.GetAxisRaw("Horizontal") * RunSpeed ;
+        Vector2 newVelocity = new Vector2(velocityX, myRigidbody.velocity.y);
+        myRigidbody.velocity = newVelocity;
+    }
+}
